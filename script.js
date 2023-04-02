bcfc38105f65b385ed01abaed458923f09ff1e23
unknown <kathyrnyoung57@gmail.com>
2023-04-02 17:13:24 -0400

    added first prompt

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var char = Number(
+    prompt ('Enter the amount of characters you would in your password please.')
+  );
+
+  if (char < 8) {
+  alert('Password must be between 8 and 128 characters.')
+  } else if (char > 128) {
+  alert('Password must be between 8 and 128 characters.')
+  }
+  // } else //(char != null) 
+  // {
+  //   alert('Please enter a number')
+  // }
+  return '';
+
+}
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +32,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+

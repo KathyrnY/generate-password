769b8415fb034d88192c83898ca20f344c3fc1bd
unknown <kathyrnyoung57@gmail.com>
2023-04-02 19:43:10 -0400

    added variables with if statments and confirms as well

--- script.js
+++ script.js
@@ -10,19 +10,43 @@ function generatePassword() {
   alert('Password must be between 8 and 128 characters.')
   } else if (char > 128) {
   alert('Password must be between 8 and 128 characters.')
+  } else (char != null) 
+    alert('Please enter a number')
   }
-  // } else //(char != null) 
-  // {
-  //   alert('Please enter a number')
-  // }
+  
   return '';
 
-}
+// assigned variables
+  var lowerCase = 'abcdefghijklmnopqrstuvwxz';
+  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numeric ='0123456789';
+  var special = '!@#$%^&*()+';
+  var genPassword = '';
+  // variable to encapsulate all variables together to generate password
+  var all = '';
+
+ if (confirm('Would you like lowercase letters in your password?')) {
+ all +=  lowerCase
+  }
+
+  if (confirm ('Would you like uppercase letters in your password?')) {
+  all += upperCase
+  }
+
+  if (confirm ('Would you like numbers in your password?')) {
+    all += numeric
+  }
+
+  if (confirm ('Would you like special characters in your password?')) {
+    all += special
+  }
+
+
 
 
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(genPassword) {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
